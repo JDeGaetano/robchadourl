a8179f28318406ff6a78f71638386274aad61da7
JDeGaetano <jddegaetano@gmail.com>
2016-01-06 21:37:14 -0800

    Update robchadourl.js

--- robchadourl.js
+++ robchadourl.js
@@ -1,4 +1,4 @@
 var url, newurl;
 url = prompt ("Please paste url");
-newurl = url.replace ("http:\/\/192.168.1.46:5000/", "http://QuickConnect.to/robchado/");
+newurl = url.replace ("http:\/\/[fe80::211:32ff:fe4a:870f]/", "http://QuickConnect.to/robchado/");
 window.prompt("Copy to Clipboard: Ctrl+C, Enter", newurl);
