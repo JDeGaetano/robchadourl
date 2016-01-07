bc7c00cabc0377f869df039e8219dbf25a808862
JDeGaetano <jddegaetano@gmail.com>
2016-01-06 22:10:53 -0800

    updated for new ipv6 address

--- robchadourl.js
+++ robchadourl.js
@@ -1,4 +1,4 @@
 var url, newurl;
 url = prompt ("Please paste url");
-newurl = url.replace ("http:\/\/[fe80::211:32ff:fe4a:870f]/", "http://QuickConnect.to/robchado/");
+newurl = url.replace ("[fe80::211:32ff:fe4a:870f]:5000/", "QuickConnect.to/robchado/");
 window.prompt("Copy to Clipboard: Ctrl+C, Enter", newurl);
